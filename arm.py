d7f54981042113bf35bc62f3a0ef8c0f42aba2f2
Uladzislau Khamkou <kham@tuta.io>
2022-08-27 07:40:12 +0300

    Add Environment dataclass

--- arm.py
+++ arm.py
@@ -1,6 +1,12 @@
 import functools
-from nodes import *
+from dataclasses import dataclass
 from typing import Iterator
+from nodes import *
+
+
+@dataclass
+class Environment:
+    locals: dict[str, int]
 
 
 def label_factory(seed: int = 0):
@@ -16,31 +22,31 @@ new_label = label_factory(1)
 
 
 @functools.singledispatch
-def emit(ast: AST) -> Iterator[str]:
+def emit(ast: AST, env: Environment) -> Iterator[str]:
     raise NotImplementedError(f"{ast}:{type(ast)}")
 
 
 @emit.register
-def _(block: Block) -> Iterator[str]:
+def _(block: Block, env: Environment) -> Iterator[str]:
     for statement in block.statements:
-        yield from emit(statement)
+        yield from emit(statement, env)
 
 
 @emit.register
-def _(f: Function) -> Iterator[str]:
+def _(f: Function, env: Environment) -> Iterator[str]:
     if f.name == "main":
         yield ".global main"
     yield f.name + ":"
     yield "  push {fp, lr}"
-    for line in emit(f.body):
+    for line in emit(f.body, env):
         yield "  " + line
     yield "  mov r0, #0"
     yield "  pop {fp, pc}"
 
 
 @emit.register
-def _(checker: Assert) -> Iterator[str]:
-    yield from emit(checker.condition)
+def _(checker: Assert, env: Environment) -> Iterator[str]:
+    yield from emit(checker.condition, env)
     yield "cmp r0, #1"
     yield "moveq r0, #'.'"
     yield "movne r0, #'F'"
@@ -48,13 +54,13 @@ def _(checker: Assert) -> Iterator[str]:
 
 
 @emit.register
-def _(number: Number) -> Iterator[str]:
+def _(number: Number, env: Environment) -> Iterator[str]:
     yield f"ldr r0, ={number.value}"
 
 
 @emit.register
-def _(node: Not) -> Iterator[str]:
-    yield from emit(node.term)
+def _(node: Not, env: Environment) -> Iterator[str]:
+    yield from emit(node.term, env)
     yield "cmp r0, #0"
     yield "moveq r0, #1"
     yield "movne r0, #0"
@@ -71,40 +77,40 @@ BINARY_OPERATIONS = {
 
 
 @emit.register
-def _(binary: BinaryOperation) -> Iterator[str]:
-    yield from emit(binary.left)
+def _(binary: BinaryOperation, env: Environment) -> Iterator[str]:
+    yield from emit(binary.left, env)
     yield "push {r0, ip}"
-    yield from emit(binary.right)
+    yield from emit(binary.right, env)
     yield "pop {r1, ip}"
     yield from BINARY_OPERATIONS[binary.operation].split("\n")
 
 
 @emit.register
-def _(call: Call) -> Iterator[str]:
+def _(call: Call, env: Environment) -> Iterator[str]:
     count = len(call.args)
     if count == 1:
-        yield from emit(call.args[0])
+        yield from emit(call.args[0], env)
     elif count > 1:
         # TODO: what about 8-byte stack alignment?
         yield f"sub sp, sp, #{4 * count - 4}"
         for i, arg in enumerate(call.args[1:], 1):
-            yield from emit(arg)
+            yield from emit(arg, env)
             yield f"str r0, [sp, #{4*i}]"
-        yield from emit(call.args[0])
+        yield from emit(call.args[0], env)
         yield "pop {" + ", ".join(f"r{i+1}" for i in range(count)) + "}"
     yield f"bl {call.calle}"
 
 
 @emit.register
-def _(if_node: If) -> Iterator[str]:
+def _(if_node: If, env: Environment) -> Iterator[str]:
     alt_label = new_label()
     end_label = new_label() if if_node.alternative != Block([]) else alt_label
-    yield from emit(if_node.conditional)
+    yield from emit(if_node.conditional, env)
     yield "cmp r0, #0"
     yield f"beq {alt_label}"
-    yield from emit(if_node.consequence)
+    yield from emit(if_node.consequence, env)
     if alt_label != end_label:
         yield f"b {end_label}"
         yield f"{alt_label}:"
-        yield from emit(if_node.alternative)
+        yield from emit(if_node.alternative, env)
     yield f"{end_label}:"
